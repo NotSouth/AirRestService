8c540cb63bf108379c41ff47eeeb08680c9fd5a6
RADU EUGEN ADUMITROAIEI <radu0210@edu.easj.dk>
2021-11-23 13:22:41 +0100

    Added azure db

--- AirRestService/Program.cs
+++ AirRestService/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,6 +14,12 @@ namespace AirRestService
     {
         public static void Main(string[] args)
         {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+            builder.DataSource = "<your_server.database.windows.net>";
+            builder.UserID = "<your_username>";
+            builder.Password = "<your_password>";
+            builder.InitialCatalog = "<your_database>";
             CreateHostBuilder(args).Build().Run();
         }
 
